bde13a1b44acbb034827c21aa03616e145e7b27c
Ludmila Ayala <105467834+ayalaludmila@users.noreply.github.com>
2023-03-30 21:15:17 -0300

    php conexion bd

---
+++ phpsqlserver/conexion.php
@@ -0,0 +1,16 @@
+<?php
+
+
+    class classConexion(){
+
+        function ConexionBD(){
+            $host = 'localhost';
+            $bdname = 'pp3_tp1';
+            $username = '';
+            $pasword = '';
+            $puerto =;
+        }
+
+    }
+
+?>
