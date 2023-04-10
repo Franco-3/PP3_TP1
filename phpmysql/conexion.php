6eba52757d45edd2da3052a682d48bf8e0dd6357
Ludmila Ayala <105467834+ayalaludmila@users.noreply.github.com>
2023-04-09 23:32:19 -0300

    phpmysql
    conexion php a base de datos mysql

---
+++ phpmysql/conexion.php
@@ -0,0 +1,27 @@
+<?php
+
+    class cConexion{
+
+        public static function conexionBD(){
+            $host = '127.0.0.1:33065';
+            $bdname = 'pp3tp1bd';
+            $usuario ='root';
+            $pasword = '';
+
+            try {
+             
+                $conn = new PDO("mysql:host=$host;bdname=$bdname",$usuario,$pasword);
+                echo("Conectado");            
+
+            } catch (\Throwable $exp) {
+                
+                echo("No se logro conectar:$bdname, error: $exp");
+            }
+
+            return $conn;
+
+        }
+
+    }
+
+?>
