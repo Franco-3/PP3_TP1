de12b2fe25d35837bd081227ab28f0b9e3eba199
Franco-3 <franco_33.33@hotmail.com>
2023-04-16 20:57:02 -0300

    13/04
    agregada pantalla principal
    logica basica de autenticacion implementada

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -1,17 +1,54 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
+const pool = require('../database');
+const helpers = require('../lib/helpers');
+
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'name',
+    passwordField: 'password',
+    passReqToCallback: true
+},async (req, username, password, done) => {
+    console.log(req.body);
+    const user = await pool.query('SELECT * FROM usuarios WHERE Correo = ?', [username]);
+    if (user.length > 0) {
+        //console.log( password);
+        //console.log( user[0].Contraseña);
+        const validPassword = await helpers.matchPassword( password, user[0].Contraseña);
+        console.log(validPassword, user[0].Contraseña);
+        if (validPassword) {
+            done(null, user, req.flash('success', 'Bienvenido/a ' + user.Nombre));
+        }
+    }else{
+        return done(null, false, req.flash('message' ,'El usuario ingresado no existe'));
+    }
+}));
+
 passport.use('local.signup', new LocalStrategy({
     usernameField: 'name',
     passwordField: 'password',
     passReqToCallback: true
 }, async (req, username, password, done) => {
-
+    const { email } = req.body;
     console.log(req.body);
+    const newUser = {
+        Nombre: username,
+        Correo: email,
+        Contraseña: password,
+        rolUsuario: 1
+    };
+    newUser.Contraseña = await helpers.encryptPassword(password);
     
+    const result = await pool.query('INSERT INTO usuarios SET ?', [newUser]);
+    newUser.idUsuario = result.insertId;
+    return done(null, newUser);
 }));
-/*
-passport.serializeUser((usr, done) => {
 
+passport.serializeUser((user, done) => {
+    done(null, user.idUsuario);
 });
-*/
+
+passport.deserializeUser(async (id, done) => {
+    const dataUser = await pool.query('SELECT * FROM usuarios WHERE idUsuario = ?', [id.idUsuario]);
+    done(null, id);
+});
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -6,16 +6,34 @@ router.get('/signup', (req, res) => {
     res.render('../register/register');
 });
 
+router.get('/signin', (req, res) => {
+    res.render('../login/login');
+});
+
+router.get('/profile', (req, res) => {
+    res.render('../user/user');
+});
+
+router.get('/user', (req, res) => {
+    res.render('../user/user');
+});
+
 router.post('/signup', passport.authenticate('local.signup',
     {
         successRedirect: '/profile',
-        failureRedirect: '/signup',
+        failureRedirect: '/users',
         failureFlash: true
-    }));
+    }
+));
 
+router.post('/signin', (req, res, next) => {
+        passport.authenticate('local.signin', {
+            successRedirect: '/user',
+            failureRedirect: '/signin',
+            failureFlash: true
+        })(req, res, next);
+    });
+   
 
-router.get('/profile', (req, res) => {
-    res.send('Este es tu perfil');
-});
 
 module.exports = router;
